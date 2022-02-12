8193859b7f5c7eb5afb3ad88ec57a42bb0547929
HisokiKurayami <hisokikurayami@gmail.com>
2022-02-12 23:47:26 +0200

    Firebase

---
+++ lib/common/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final ThemeData theme = ThemeData(
+  scaffoldBackgroundColor: const Color(0xFF35858B),
+  textTheme: GoogleFonts.poppinsTextTheme(),
+);
