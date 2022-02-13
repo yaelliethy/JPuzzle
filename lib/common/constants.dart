f9930849a50cc120ce4b5654c8130f31b1c19b47
HisokiKurayami <hisokikurayami@gmail.com>
2022-02-13 23:51:51 +0200

    Constants

---
+++ lib/common/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kBackgroundColor = Color(0xFF810034);
+
+const Color kPrimaryColor = Color(0xFFFF005C);
+
+const Color kTextColor = Color(0xFFFFF600);
