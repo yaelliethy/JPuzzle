3b6486261a2040804db0919a0eea0f84ab8e2002
HisokiKurayami <hisokikurayami@gmail.com>
2022-02-14 00:05:23 +0200

    Added Assets

--- lib/models/Game.dart
+++ lib/models/Game.dart
@@ -1,18 +1,25 @@
 //Game model with time, dimension and score
-class Game{
+class Game {
   final int id;
   final int time;
   final int dimension;
   final int score;
 
-  Game({required this.id, required this.time, required this.dimension, required this.score});
+  Game({
+    required this.id,
+    required this.time,
+    required this.dimension,
+    required this.score,
+  });
 
-  factory Game.fromJson(Map<String, dynamic> json){
+  factory Game.fromJson(
+    Map<String, dynamic> json,
+  ) {
     return Game(
       id: json['id'],
       time: json['time'],
       dimension: json['dimension'],
-      score: json['score']
+      score: json['score'],
     );
   }
 }
